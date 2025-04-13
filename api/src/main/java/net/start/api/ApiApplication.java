4adaf5cc479f5a19a4b31a6af5a39a6ccf9815a8
solix <solixnakano@gmail.com>
2025-04-13 23:28:51 +0300

    frontend->update

--- api/src/main/java/net/start/api/ApiApplication.java
+++ api/src/main/java/net/start/api/ApiApplication.java
@@ -2,6 +2,7 @@ package net.start.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class ApiApplication {
