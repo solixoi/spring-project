5b4f77ad992a23455abd4054dcda199225685f8b
solix <solixnakano@gmail.com>
2025-04-13 23:25:53 +0300

    added Repo

---
+++ api/src/main/java/net/start/api/repo/RoleRepo.java
@@ -0,0 +1,8 @@
+package net.start.api.repo;
+
+import net.start.api.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepo extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
---
+++ api/src/main/java/net/start/api/repo/UserRepo.java
@@ -0,0 +1,8 @@
+package net.start.api.repo;
+
+import net.start.api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
